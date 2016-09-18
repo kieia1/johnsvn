6a35aa236c945574f47ad3384f6b525c0da3d9c0
kieia1 <kiei@mailinator.com>
2016-09-18 20:37:43 +0200

    Ahora pide 10 num, max y min

--- NumMesPetitDeCinc.java
+++ NumMesPetitDeCinc.java
@@ -1,15 +1,16 @@
-;
 import java.util.Scanner;
 
 public class NumMesPetitDeCinc {
 
     public static void main(String... args) {
         Scanner in = new Scanner(System.in);
-        int min = in.nextInt();
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 10; i++) {
             int aux = in.nextInt();
             min = aux < min ? aux : min;
+            max = aux > max ? aux : max;
         }
 
         System.out.println(min);
